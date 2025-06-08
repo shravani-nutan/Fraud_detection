6665b5b1178f3344ff6c9abee2b3ec1c2b397a65
shravani <132260098+shravani-nutan@users.noreply.github.com>
2025-06-08 15:52:38 +0530

    Update Fraud_detection_model.py

--- Fraud_detection_model.py
+++ Fraud_detection_model.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 import joblib
-model=joblib.load("fraud_detection_pipeline.plk")
+model=joblib.load("fraud_detection_pipeline.pkl")
 
 st.title(" fraud dtetection predection model")
 
